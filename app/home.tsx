5fedceefffc67d3424aa641f41dbf71544439a46
Roman <roman.for.programing@gmail.com>
2025-05-11 23:09:35 +0200

    SmallFixs

--- app/home.tsx
+++ app/home.tsx
@@ -1,4 +1,4 @@
-import {View, Text, TouchableOpacity, StyleSheet, ScrollView} from 'react-native';
+import { View, Text, TouchableOpacity, StyleSheet } from 'react-native';
 import { Theme, useTheme } from '@/src/contexts/ThemeContext';
 import WaveBackground from "@/src/components/WaveBackground";
 import { useCallback, useEffect, useState } from 'react';
@@ -16,7 +16,8 @@ import { registerTrainingReminderTask, BACKGROUND_NOTIFICATION_TASK } from '@/sr
 import { widthPercentageToDP as wp, heightPercentageToDP as hp } from 'react-native-responsive-screen';
 import getGlobalStyle from '@/src/styles/Global';
 import { clearDatabase, TrainingUserDTO } from '@/src/db/init';
-
+import { init as initDB } from '@/src/db/init';
+import * as TaskManager from 'expo-task-manager';
 
 const UPCOMING_TRAININGS_CNT = 2;
 
@@ -24,8 +25,6 @@ export default function Home() {
     const { theme, toggleTheme } = useTheme();
     const style = getStyle(theme);
     const styles = getGlobalStyle(theme);
-    const [errorMessage, setErrorMessage] = useState('');
-    const [isErrorPopupVisible, setIsErrorPopupVisible] = useState(false);
     const cancelTrainingError = (e: any) => {
         Toast.show({
             type: 'error',
@@ -49,14 +48,16 @@ export default function Home() {
     const [invitations, setInvitations] = useState<Training[]>([]);
     const [trainingToCancel, setTrainingToCancel] = useState<Training|null>(null);
 
-    async function init() {
-        try {
-            const upcoming: Training[] = await getUpcomingLocal();
-            setTrainings(upcoming.slice(0, UPCOMING_TRAININGS_CNT));
-            if (role === 'USER')
-                setInvitations(await getInvitationsLocal());
+    async function _init() {
+        const role = (await getRole()) || 'USER';
+        await initDB(role as Role);
+        const upcoming: Training[] = await getUpcomingLocal();
+        console.log("Upcoming: ", upcoming);
+        setTrainings(upcoming.slice(0, UPCOMING_TRAININGS_CNT));
+    }
 
-            console.log(await AsyncStorage.getItem('token'));
+    async function initSettings() {
+        try {
             // Спроба отримати налаштування з бекенду
             const response = await api.get("/settings/user");
             console.log(response.data);
@@ -86,6 +87,24 @@ export default function Home() {
             }
         }
     }
+
+    useEffect(() => {
+        async function q() {
+            _init();
+            initSettings();
+            await registerTrainingReminderTask();
+            // await TaskManager. (BACKGROUND_NOTIFICATION_TASK);
+        } 
+    }, []);
+
+    async function init() {
+        const upcoming: Training[] = await getUpcomingLocal();
+        console.log("Upcoming: ", upcoming);
+        setTrainings(upcoming.slice(0, UPCOMING_TRAININGS_CNT));
+        if (role === 'USER')
+            setInvitations(await getInvitationsLocal());
+    }
+
     async function handleLogout() {
         try {
             await setAuthToken('');
@@ -102,7 +121,6 @@ export default function Home() {
     useFocusEffect(
         useCallback(() => {
             init();
-            registerTrainingReminderTask();
             return () => {};
         }, [])
     );
@@ -138,138 +156,96 @@ export default function Home() {
     return (
         <View style={style.container}>
             <WaveBackground />
-            <ScrollView
-                style={{ flex: 1 }} // Додаємо flex: 1 для ScrollView
-                contentContainerStyle={{
-                    paddingHorizontal: wp('5%'),
-                    paddingVertical: hp('2%'),
-                    paddingBottom: hp('15%'), // Збільшуємо paddingBottom, щоб уникнути перекриття
-                }}
-                keyboardShouldPersistTaps="handled"
-                showsVerticalScrollIndicator={true} // Додаємо індикатор прокрутки для зручності
-            >
-                <View style={{
-                    width: '100%',
-                    alignItems: 'center',
-                    marginTop: hp('4%')
-                }}>
-                    <View style={{
-                        width: '95%',
-                        padding: 1,
-                        borderWidth: 1,
-                        borderRadius: 20,
-                        borderColor: theme.borderColor,
-                        backgroundColor: theme.background,
-                        alignItems: 'center'
-                    }}>
-                        {trainings.map((training, idx) => (
-                            <TrainingCard
-                                key={idx}
-                                training={training}
-                                onDelete={(training: Training) => { setTrainingToCancel(training) }}
-                            />
-                        ))}
-                        <Link href="/trainings/upcoming-trainings" asChild>
-                            <TouchableOpacity activeOpacity={0.8}>
-                                <View style={{
-                                    backgroundColor: theme.buttonBackground,
-                                    padding: 4,
-                                    borderRadius: 3,
-                                    marginVertical: hp('1%')
-                                }}>
-                                    <Text style={[style.buttonText, { color: theme.buttonText, fontSize: 15 }]}>
-                                        Full Schedule
-                                    </Text>
-                                </View>
-                            </TouchableOpacity>
-                        </Link>
-                    </View>
-                    <View>
-                        <TrainingAttendance />
-                    </View>
 
-                    {role === 'COACH' &&
-                        <TouchableOpacity
-                            activeOpacity={0.8}
-                            onPress={() => { router.push("/trainings/create-training") }}
+            <View style={{width: '95%', padding: 1, borderWidth: 1, borderRadius: 20, borderColor: theme.borderColor, backgroundColor: theme.background, alignItems: 'center'}}>
+                {trainings.map((training, idx) => {
+                    return <TrainingCard key={idx} training={training} onDelete={(training: Training) => {setTrainingToCancel(training)}}></TrainingCard>
+                })}
+                <Link href="/trainings/upcoming-trainings" asChild>
+                    <TouchableOpacity activeOpacity={0.8}>
+                        <View
+                            style={{backgroundColor: theme.buttonBackground, padding: 4, borderRadius: 3}}
                         >
-                            <View style={[style.button, { backgroundColor: theme.buttonBackground, borderColor: theme.borderColor }]}>
-                                <Text style={[style.buttonText, { color: theme.buttonText }]}>
-                                    Create Training
-                                </Text>
-                            </View>
-                        </TouchableOpacity>
-                    }
-
-                    {role === 'USER' && <>
-                        <Text style={styles.titleText}>Invitations</Text>
-                        {invitations.map((t, idx) => (
-                            <TrainingCard
-                                key={idx}
-                                training={t}
-                                onDelete={denyInvitation}
-                                onInvite={acceptInvitation}
-                                isInvitation={true}
-                            />
-                        ))}
-                        <TrainingCancelConfirm
-                            training={trainingToCancel}
-                            setTraining={setTrainingToCancel}
-                            onPress={cancelTraining}
-                        />
-                    </>}
-                </View>
-            </ScrollView>
+                            <Text style={[style.buttonText, { color: theme.buttonText, fontSize: 15 }]}>Full Schedule</Text>
+                        </View>
+                    </TouchableOpacity>
+                </Link>
+            </View>
+            <View>
+                <TrainingAttendance />
+            </View>
+            
+            {role === 'COACH' && 
+                <TouchableOpacity activeOpacity={0.8} onPress={() => {router.push("/trainings/create-training")}}>
+                    <View
+                        style={[style.button, { backgroundColor: theme.buttonBackground, borderColor: theme.borderColor }]}
+                    >
+                        <Text style={[style.buttonText, { color: theme.buttonText }]}>CreateTraining</Text>
+                    </View>
+                </TouchableOpacity>}
             <BottomNavigation />
+
+            {role === 'USER' && <>
+                <Text style={styles.titleText}>Invitations</Text>
+                {invitations.map((t, idx) => 
+                    <TrainingCard key={idx} 
+                        training={t} 
+                        onDelete={denyInvitation} 
+                        onInvite={acceptInvitation} 
+                        isInvitation={true} />)}
+
+                <TrainingCancelConfirm training={trainingToCancel} setTraining={setTrainingToCancel} onPress={cancelTraining} />
+            </>}
+
+            {role === 'USER' && <TrainingCancelConfirm training={trainingToCancel} setTraining={setTrainingToCancel} onPress={cancelTraining} />}
             <Toast />
         </View>
-    );
+    )
 }
 
-export const getStyle = (theme: Theme) => {
-    return StyleSheet.create({
-        container: {
-            flex: 1,
-            alignItems: 'center',
-            justifyContent: 'flex-start',
-            backgroundColor: theme.background,
-        },
-        image: {
-            width: wp('70%'),
-            height: hp('35%'),
-            borderRadius: wp('3%'),
-            borderWidth: 2,
-            marginTop: hp('5%'),
-        },
-        textContainer: {
-            alignItems: 'center',
-            paddingHorizontal: wp('5%'),
-            marginVertical: hp('3%'),
-        },
-        text: {
-            fontSize: wp('7%'),
-            fontWeight: 'bold',
-            textAlign: 'center',
-            fontFamily: 'InriaSerif-Regular',
-        },
-        buttonContainer: {
-            width: '80%',
-            marginBottom: hp('5%'),
-            alignItems: 'center',
-        },
-        button: {
-            width: wp('70%'),
-            height: hp('8%'),
-            borderRadius: wp('3%'),
-            marginVertical: hp('1.5%'),
-            alignItems: 'center',
-            justifyContent: 'center',
-            borderWidth: 2,
-        },
-        buttonText: {
-            fontSize: wp('5.5%'),
-            fontWeight: 'bold',
-            fontFamily: 'InriaSerif-Regular',
-        },
-    });
-};
+export const getStyle = (theme: Theme) => {return StyleSheet.create({
+    container: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'flex-start',
+        paddingVertical: hp('7%'),
+        backgroundColor: theme.background
+    },
+    image: {
+        width: wp('70%'),
+        height: hp('35%'),
+        borderRadius: wp('3%'),
+        borderWidth: 2,
+        marginTop: hp('5%'),
+    },
+    textContainer: {
+        alignItems: 'center',
+        paddingHorizontal: wp('5%'),
+        marginVertical: hp('3%'),
+    },
+    text: {
+        fontSize: wp('7%'),
+        fontWeight: 'bold',
+        textAlign: 'center',
+        fontFamily: 'InriaSerif-Regular',
+    },
+    buttonContainer: {
+        width: '80%',
+        marginBottom: hp('5%'),
+        alignItems: 'center',
+    },
+    button: {
+        width: wp('70%'),
+        height: hp('8%'),
+        borderRadius: wp('3%'),
+        marginVertical: hp('1.5%'),
+        alignItems: 'center',
+        justifyContent: 'center',
+        borderWidth: 2,
+    },
+    buttonText: {
+        fontSize: wp('5.5%'),
+        fontWeight: 'bold',
+        fontFamily: 'InriaSerif-Regular',
+    },
+});}
